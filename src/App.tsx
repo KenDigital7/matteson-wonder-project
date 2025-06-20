3dc5014ff73d8f75b5be3ee3e53ebe633fe2afa7
Ken Williams <info@kenctures.com>
2025-06-20 17:58:53 -0500

    Fix routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/matteson-wonder-project">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/leadership" element={<Leadership />} />
